d13fcd1802c41cb19afb9a6ad9dc1cefce45c359
elwolf <elwolf6@protonmail.com>
2021-11-09 21:20:33 -0500

    Update runit-autism.sh

--- runit-autism.sh
+++ runit-autism.sh
@@ -11,7 +11,7 @@ mkdir -p /etc/runit/sv/ckb-next-daemon
 printf '#!bin/sh
 exec ckb-next-daemon > /dev/null 2>&1' > /etc/runit/sv/ckb-next-daemon/run
 chmod +x /etc/runit/sv/ckb-next-daemon/run
-ckb-next-daemon > /dev/null 2>&1 && killall ckb-next-daemon
+ckb-next-daemon > /dev/null 2>&1 & killall ckb-next-daemon
 ln -s /etc/runit/sv/ckb-next-daemon /run/runit/service
 sv up ckb-next-daemon
 killall NetworkManager
