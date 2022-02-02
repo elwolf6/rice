374602969eee63a487d4046701dad976018451d5
elwolf <elwolf6@protonmail.com>
2022-02-01 21:31:58 -0500

    pipewire-autism

--- runit-autism.sh
+++ runit-autism.sh
@@ -17,6 +17,18 @@ sv up ckb-next-daemon
 killall NetworkManager
 sv up NetworkManager
 
+mkdir -p /etc/runit/sv/pipewire-autism
+printf '#!/bin/sh
+pipewire &
+pipewire-pulse &
+pipewire-media-session &' > /etc/runit/sv/pipewire-autism/run
+chmod +x /etc/runit/sv/runit-autism/run
+pipewire > /dev/null 2>&1 & killall pipewire
+pipewire-pulse > /dev/null 2>&1 & killall pipewire-pulse
+pipewire-media-session > /dev/null 2>&1 & killall pipewire-media-session
+ln -s /etc/runit/sv/pipewire-autism /run/runit/service
+sv up pipewire-autism
+
 rm -rf /home/elwolf/go
 rm runit-autism.sh
 shutdown -r now
