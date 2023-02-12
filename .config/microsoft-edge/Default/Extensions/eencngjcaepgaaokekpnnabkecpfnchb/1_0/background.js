000fc1ba0585ab3273993e7739a9781c4e9ddc18
luis <elwolf6@protonmail.com>
2023-02-11 19:48:38 -0500

    terminology and no edge no warg replays etc

--- .config/microsoft-edge/Default/Extensions/eencngjcaepgaaokekpnnabkecpfnchb/1_0/background.js
+++
@@ -1,4 +0,0 @@
-function sauce(info, tab){ chrome.tabs.create({ "url": "http://saucenao.com/search.php?url="+escape(info.srcUrl), selected: false }); }
-function scanModul(info, tab){}
-chrome.contextMenus.removeAll();
-chrome.contextMenus.create({"title": "Get Sauce", "contexts": [ "image" ], "onclick": sauce});
